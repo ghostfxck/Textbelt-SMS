fc9eddcde27d332d33439cbcbb0406d7d73ebc80
ghostfxck <47380928+ghostfxck@users.noreply.github.com>
2022-05-30 02:39:47 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+const num = document.getElementById("num")
+const msg = document.getElementById("msg")
+const key = document.getElementById("key")
+
+function send() {
+    fetch('https://textbelt.com/text', {
+  method: 'post',
+  headers: { 'Content-Type': 'application/json' },
+  body: JSON.stringify({
+    phone: `"${num}"`,
+    message: `"${msg}"`,
+    key: `"${key}"`,
+  }),
+}).then(response => {
+  return response.json();
+}).then(data => {
+  console.log(data);
+});
+}
